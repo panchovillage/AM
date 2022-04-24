c1c86c87d4ac71c71517993d19a2be1dfba44b44
panchovillage <casimillion7@gmail.com>
2022-04-24 21:44:05 +0100

    commit 5

--- RestAPI/index.js
+++ RestAPI/index.js
@@ -1,4 +1,7 @@
+<<<<<<< HEAD
 <<<<<<< Updated upstream
+=======
+>>>>>>> b996069 (commit 5)
         const express = require('express');
         const bodyParser = require('body-parser');
         const res = require('express/lib/response');
@@ -10,6 +13,7 @@
         })
 
 
+<<<<<<< HEAD
 
         /*
         Lista de endpoints da aplicação CRUD de mensagens
@@ -231,6 +235,116 @@ app.delete('/mensagens/:id', (req, res) => {
     delete mensagens[index];
     res.send('Mensagem removida com sucesso.');
 });
+=======
+
+        /*
+        Lista de endpoints da aplicação CRUD de mensagens
+        - [GET] /mensagens - Retorna a lista de mensgens
+        - [GET] /mensagens{id} - Retorna uma mensgem pelo ID
+        - [GET] /mensagens/{id} - Atualiza msg pelo ID
+        - [POST] /mensagens - Cria uma nova mensagem
+        - [PUT] /mensagens/{id} - Atualiza msg pelo ID
+        - [DELETE] /mensagens/{id} - Remover uma msg pelo ID
+
+        */
+
+        const mensagens = [
+        {
+          "id":1,
+          "texto":  "Essa é a primeira mensagem"
+        } ,
+        
+        {
+          "id":2,
+          "texto": "Essa é a segunda mensagem"
+
+        },
+          ];
+       
+
+const getMensagensValidas = () => mensagens.filter(Boolean);
+
+const getMensagemById = id =>  getMensagensValidas().find(msg => msg.id === id);
+
+
+        //[GET] /mensagens - Retorna a lista de mensgens
+          app.get('/mensagens', (req, res) => {
+          res.send(getMensagensValidas()); 
+          });
+
+        // - [GET] /mensagens{id} - Retorna uma mensgem pelo ID
+        app.get('/mensagens/:id', (req, res) => {
+        const id = +req.params.id ;
+        const mensagem = getMensagemById(id); 
+        
+        if(!mensagem){
+          res.send('Mensagem não encontrada');
+
+          return;
+        }
+          res.send(mensagem);
+        });
+
+        //- [POST] /mensagens - Cria uma nova mensagem
+        app.post('/mensagens', (req, res) => {
+        const mensagem = req.body;
+
+        if(!mensagem || !mensagem.texto){
+          res.send('Mensagem inválida')
+        return
+        }
+
+
+        mensagem.id = mensagens.length + 1;
+        mensagens.push(mensagem);
+        //console.log(mensagem);
+
+        res.send(mensagem)          
+        });
+
+        //- [PUT] /mensagens/{id} - Atualiza msg pelo ID
+        app.put('/mensagens/:id', (req, res) => {
+          const id = +req.params.id ;
+
+          //console.log(typeof id, typeof mensagens[0].id);
+        
+          const mensagem = getMensagemById(id);
+          const novTexto = req.body.texto;
+
+        if (!novTexto){
+          res.send('Mensagem inválida');
+          return;
+        }
+
+        mensagem.texto = novTexto;
+        //mensagens[id] = mensagem;
+
+          res.send(mensagem);  });
+
+        //- [DELETE] /mensagens/{id} - Remover uma msg pelo ID
+      app.delete('/mensagens/:id', (req, res) => {
+      const id = +req.params.id ;
+
+      const mensagem = getMensagemById(id);
+        
+        if(!mensagem){
+          res.send('ID inválido');
+          return;
+        }
+
+      const index = mensagens.indexOf(mensagem);
+      delete mensagens[index];
+
+      res.send('Mensagem removida com sucesso');
+       
+
+        });
+
+        app.listen(port, () =>{
+        console.info('App está a executar em: http://localhost:${port}')
+
+        });
+>>>>>>> b996069 (commit 5)
 
 app.listen(port, () => {
     console.info(`App rodando em http://localhost:${port}`);
