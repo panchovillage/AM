e2f8353ba79294160cf51f1c53bc5297dd79e106
panchovillage <casimillion7@gmail.com>
2022-04-24 03:10:20 +0100

    second

--- RestAPI/index.js
+++ RestAPI/index.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+const res = require('express/lib/response');
 const app = express()
 const port = 3000;
 app.get('/hello', (req, res) => {
@@ -9,7 +10,7 @@ app.get('/hello', (req, res) => {
 
 /*
 Lista de endpoints da aplicação CRUD de mensagens
-- [GET] /mensagens - Retorna a lista de mensgaens
+- [GET] /mensagens - Retorna a lista de mensgens
 - [GET] /mensagens/{id} - Atualiza msg pelo ID
 - [POST] /mensagens - Cria uma nova mensagem
 - [PUT] /mensagens/{id} - Atualiza msg pelo ID
@@ -20,17 +21,26 @@ Lista de endpoints da aplicação CRUD de mensagens
 const mensagens = [
   "Essa é a primeira mensagem", 
   "Essa é a segunda mensagem"
-  ]
+  ];
+//mensagens[0]
 
+//[GET] /mensagens - Retorna a lista de mensgens
+  app.get('/mensagens', (req, res) => {
+  res.send(mensagens); 
+  });
 
+// - [GET] /mensagens{id} - Retorna uma mensgem pelo ID
+app.get('/mensagens', (req, res) => {
+ const id = 0;
 
-
-
+ const mensagem = mensagens[id];
+ 
+  res.send(mensagem);
+});
 
 
 app.listen(port, function(){
-
 console.info('App está a executar em: http://localhost:${port}')
 
-})
+});
 
